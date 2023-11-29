83c2257f8587f9c47b5b791e27c3efd2e4b0fe68
Ryan Mulcahy <mulchy81@gmail.com>
2023-11-29 16:02:04 -0600

    Revert "Implement day 1 last year"
    This reverts commit 3a45c13bb59c80586b85277aeec489a575d49b35.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,5 @@
 use advent::io;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 
 fn main() -> Result<()> {
     let input = io::for_day(1)?;
@@ -8,36 +8,12 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn part1(input: &str) -> Result<i32> {
-    let masses = calculate_masses_for_groups(input)?;
-
-    let max = masses.iter().max().ok_or(anyhow::anyhow!("No max found"))?;
-
-    Ok(*max)
+fn part1(_input: &str) -> Result<String> {
+    unimplemented!("You have to solve the puzzle first!")
 }
 
-fn part2(input: &str) -> Result<i32> {
-    let mut masses = calculate_masses_for_groups(input)?;
-
-    masses.sort();
-    masses.reverse();
-
-    let top3 = masses.get(0..3).ok_or(anyhow!("Not enough data"))?;
-
-    Ok(top3.iter().sum())
-}
-
-fn calculate_masses_for_groups(input: &str) -> Result<Vec<i32>, anyhow::Error> {
-    let masses = input
-        .split("\n\n")
-        .map(|group| {
-            group
-                .lines()
-                .map(|s| Ok(s.parse::<i32>()?))
-                .sum::<Result<i32>>()
-        })
-        .collect::<Result<Vec<i32>>>()?;
-    Ok(masses)
+fn part2(_input: &str) -> Result<String> {
+    unimplemented!("You have to solve the puzzle first!")
 }
 
 #[cfg(test)]
@@ -45,46 +21,22 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn test_part1() -> Result<()> {
-        let example_input = "1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-        let expected_output = 24000;
+        let example_input = "";
+        let expected_output = "";
 
         assert_eq!(part1(example_input)?, expected_output);
         Ok(())
     }
 
     #[test]
+    #[ignore]
     fn test_part2() -> Result<()> {
-        let example_input = r"1000
-2000
-3000
+        let example_input = "";
+        let expected_output = "";
 
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-        let expected_output = 45000;
-
-        assert_eq!(part2(example_input)?, expected_output);
+        assert_eq!(part1(example_input)?, expected_output);
         Ok(())
     }
 }
