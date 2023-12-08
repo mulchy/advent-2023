c4c2fbcdf24ab113a9f778651b0ea41d20dd7c3c
Ryan Mulcahy <mulchy81@gmail.com>
2023-12-08 00:38:00 -0600

    7 & 8 pt 1

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,5 +1,13 @@
+use std::{
+    cmp::Ordering::{Equal, Greater, Less},
+    collections::HashMap,
+    fmt::Debug,
+    hash::Hash,
+    str::FromStr,
+};
+
 use advent::io;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 fn main() -> Result<()> {
     let input = io::for_day(7)?;
@@ -8,35 +16,281 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn part1(_input: &str) -> Result<String> {
-    unimplemented!("You have to solve the puzzle first!")
+trait Card: PartialEq + Eq + PartialOrd + Ord + Copy + Clone + Hash + Debug + FromStr {}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Hash)]
+enum CardWithoutJoker {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
 }
 
-fn part2(_input: &str) -> Result<String> {
-    unimplemented!("You have to solve the puzzle first!")
+impl Card for CardWithoutJoker {}
+
+impl FromStr for CardWithJoker {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "2" => Ok(Self::Two),
+            "3" => Ok(Self::Three),
+            "4" => Ok(Self::Four),
+            "5" => Ok(Self::Five),
+            "6" => Ok(Self::Six),
+            "7" => Ok(Self::Seven),
+            "8" => Ok(Self::Eight),
+            "9" => Ok(Self::Nine),
+            "T" => Ok(Self::Ten),
+            "J" => Ok(Self::Joker),
+            "Q" => Ok(Self::Queen),
+            "K" => Ok(Self::King),
+            "A" => Ok(Self::Ace),
+            _ => Err(anyhow::anyhow!("Invalid card: {}", s)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Hash)]
+enum CardWithJoker {
+    Joker,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Card for CardWithJoker {}
+
+impl FromStr for CardWithoutJoker {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "2" => Ok(Self::Two),
+            "3" => Ok(Self::Three),
+            "4" => Ok(Self::Four),
+            "5" => Ok(Self::Five),
+            "6" => Ok(Self::Six),
+            "7" => Ok(Self::Seven),
+            "8" => Ok(Self::Eight),
+            "9" => Ok(Self::Nine),
+            "T" => Ok(Self::Ten),
+            "J" => Ok(Self::Jack),
+            "Q" => Ok(Self::Queen),
+            "K" => Ok(Self::King),
+            "A" => Ok(Self::Ace),
+            _ => Err(anyhow::anyhow!("Invalid card: {}", s)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+enum Rank {
+    HighCard,
+    OnePair,
+    TwoPairs,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(Debug, Clone)]
+struct Hand<T: Card> {
+    cards: Vec<T>,
+    rank: Rank,
+}
+
+impl<T: Card> Hand<T> {}
+
+impl<T: Card> Ord for Hand<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.rank.cmp(&other.rank) {
+            Less => Less,
+            Greater => Greater,
+            Equal => self.cards.cmp(&other.cards),
+        }
+    }
+}
+
+impl<T: Card> PartialOrd for Hand<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: Card> PartialEq for Hand<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Equal
+    }
+}
+
+impl<T: Card> Eq for Hand<T> {}
+
+// todo specialize for joker
+fn rank_hand<T: Card>(cards: &[T]) -> Result<Rank> {
+    let counts = cards.iter().fold(HashMap::new(), |mut acc, card| {
+        *acc.entry(card).or_insert(0) += 1;
+        acc
+    });
+
+    let mut dupes: Vec<&i32> = counts.values().collect();
+    dupes.sort();
+
+    match dupes.as_slice() {
+        [1, 1, 1, 1, 1] => Ok(Rank::HighCard),
+        [1, 1, 1, 2] => Ok(Rank::OnePair),
+        [1, 2, 2] => Ok(Rank::TwoPairs),
+        [1, 1, 3] => Ok(Rank::ThreeOfAKind),
+        [2, 3] => Ok(Rank::FullHouse),
+        [1, 4] => Ok(Rank::FourOfAKind),
+        [5] => Ok(Rank::FiveOfAKind),
+        _ => Err(anyhow!("Invalid hand: {:?}", cards)),
+    }
+}
+
+impl<T: Card> FromStr for Hand<T> {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let cards: Vec<T> = s
+            .chars()
+            .map(|c| {
+                c.to_string().as_str().parse::<T>().map_err(|_e| {
+                    anyhow::anyhow!("idk enough about rust to coerce this error to the right type")
+                })
+            })
+            .collect::<Result<Vec<T>>>()?;
+
+        let rank = rank_hand(&cards)?;
+
+        Ok(Self { cards, rank })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+struct HandAndBid<T: Card> {
+    hand: Hand<T>,
+    bid: i32,
+}
+
+impl<T: Card> FromStr for HandAndBid<T> {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let mut parts = s.split_ascii_whitespace();
+        let hand = parts.next().ok_or(anyhow!("No data"))?;
+        let bid = parts.next().ok_or(anyhow!("No data"))?.parse()?;
+
+        Ok(Self {
+            hand: hand.parse()?,
+            bid,
+        })
+    }
+}
+
+fn do_it<T: Card>(input: &str) -> Result<i32> {
+    let mut hands_and_bids = input
+        .lines()
+        .map(HandAndBid::<T>::from_str)
+        .collect::<Result<Vec<_>>>()?;
+    hands_and_bids.sort();
+
+    Ok(hands_and_bids
+        .iter()
+        .zip(1..)
+        .map(|(hand, rank)| rank * hand.bid)
+        .sum())
+}
+
+fn part1(input: &str) -> Result<i32> {
+    do_it::<CardWithoutJoker>(input)
+}
+
+fn part2(input: &str) -> Result<i32> {
+    do_it::<CardWithJoker>(input)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::Rank::*;
     use super::*;
 
     #[test]
-    #[ignore]
-    fn test_part1() -> Result<()> {
-        let example_input = "";
-        let expected_output = "";
+    fn test_hand_from_str() -> Result<()> {
+        let input = vec![
+            ("32T3K", OnePair),
+            ("T55J5", ThreeOfAKind),
+            ("KK677", TwoPairs),
+            ("KTJJT", TwoPairs),
+            ("QQQJA", ThreeOfAKind),
+        ];
 
-        assert_eq!(part1(example_input)?, expected_output);
+        for (hand, expected_rank) in input {
+            let hand = hand.parse::<Hand<CardWithoutJoker>>()?;
+            assert_eq!(hand.rank, expected_rank);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_hand_and_bid_from_str() -> Result<()> {
+        let input = "32T3K 765";
+
+        assert_eq!(
+            input.parse::<HandAndBid<CardWithoutJoker>>()?,
+            HandAndBid::<CardWithoutJoker> {
+                hand: Hand {
+                    cards: vec![
+                        CardWithoutJoker::Three,
+                        CardWithoutJoker::Two,
+                        CardWithoutJoker::Ten,
+                        CardWithoutJoker::Three,
+                        CardWithoutJoker::King
+                    ],
+                    rank: OnePair,
+                },
+                bid: 765,
+            }
+        );
+        Ok(())
+    }
+
+    const EXAMPLE_INPUT: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        assert_eq!(part1(EXAMPLE_INPUT)?, 6440);
         Ok(())
     }
 
     #[test]
     #[ignore]
     fn test_part2() -> Result<()> {
-        let example_input = "";
-        let expected_output = "";
-
-        assert_eq!(part2(example_input)?, expected_output);
+        assert_eq!(part2(EXAMPLE_INPUT)?, 5905);
         Ok(())
     }
 }
--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,5 +1,7 @@
+use std::{collections::HashMap, str::FromStr};
+
 use advent::io;
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 fn main() -> Result<()> {
     let input = io::for_day(8)?;
@@ -8,35 +10,189 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn part1(_input: &str) -> Result<String> {
-    unimplemented!("You have to solve the puzzle first!")
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Direction {
+    Right,
+    Left,
 }
 
-fn part2(_input: &str) -> Result<String> {
-    unimplemented!("You have to solve the puzzle first!")
+#[derive(Debug, PartialEq)]
+struct Instruction {
+    directions: Vec<Direction>,
+    map: HashMap<String, (String, String)>,
+}
+
+impl FromStr for Instruction {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let parts = s.split("\n\n").collect::<Vec<_>>();
+        let [directions, map] = parts.as_slice() else {
+            bail!("Invalid input {}", s);
+        };
+
+        let directions = directions
+            .chars()
+            .map(|c| match c {
+                'R' => Ok(Direction::Right),
+                'L' => Ok(Direction::Left),
+                _ => bail!("Invalid input {}", s),
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        // AAA = (BBB, CCC)
+        let map = map
+            .lines()
+            .map(|l| {
+                let parts = l.split(" = ").collect::<Vec<_>>();
+                let [key, value] = parts.as_slice() else {
+                    bail!("Invalid input {}", s);
+                };
+
+                let value = value.to_string().replace(['(', ')'], "");
+                let parts = value.split(", ").collect::<Vec<_>>();
+
+                let [left, right] = parts.as_slice() else {
+                    bail!("Invalid input {}", s);
+                };
+
+                Ok((key.to_string(), (left.to_string(), right.to_string())))
+            })
+            .collect::<Result<HashMap<_, _>>>()?;
+
+        Ok(Self { directions, map })
+    }
+}
+
+fn walk(instructions: Instruction) -> usize {
+    let mut current = "AAA".to_string();
+    let mut count = 0;
+
+    loop {
+        let (left, right) = instructions.map.get(&current).unwrap();
+        let direction = instructions.directions[count % instructions.directions.len()];
+
+        current = match direction {
+            Direction::Right => right.to_string(),
+            Direction::Left => left.to_string(),
+        };
+
+        count += 1;
+
+        if current == "ZZZ" {
+            break;
+        }
+    }
+
+    count
+}
+
+fn ghost_walk(instructions: Instruction) -> usize {
+    let mut current = instructions
+        .map
+        .keys()
+        .filter(|s| s.ends_with('A'))
+        .cloned()
+        .collect::<Vec<_>>();
+    println!("{:?}", current);
+    let mut count = 0;
+
+    loop {
+        current = current
+            .iter()
+            .map(|s| {
+                let (left, right) = instructions.map.get(s).unwrap();
+                let direction = instructions.directions[count % instructions.directions.len()];
+
+                match direction {
+                    Direction::Right => right.to_string(),
+                    Direction::Left => left.to_string(),
+                }
+            })
+            .collect::<Vec<_>>();
+
+        count += 1;
+
+        println!("{:?}, {:?}", current, count);
+        if current.iter().all(|s| s.ends_with('Z')) {
+            break;
+        }
+    }
+
+    count
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let instructions = Instruction::from_str(input)?;
+    Ok(walk(instructions))
+}
+
+fn part2(input: &str) -> Result<usize> {
+    let instructions = Instruction::from_str(input)?;
+    Ok(ghost_walk(instructions))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    #[ignore]
-    fn test_part1() -> Result<()> {
-        let example_input = "";
-        let expected_output = "";
+    const EXAMPLE_INPUT: &str = "RL
 
-        assert_eq!(part1(example_input)?, expected_output);
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+    #[test]
+    fn test_from_str() -> Result<()> {
+        let expected = Instruction {
+            directions: vec![Direction::Right, Direction::Left],
+            map: vec![
+                ("AAA".to_string(), ("BBB".to_string(), "CCC".to_string())),
+                ("BBB".to_string(), ("DDD".to_string(), "EEE".to_string())),
+                ("CCC".to_string(), ("ZZZ".to_string(), "GGG".to_string())),
+                ("DDD".to_string(), ("DDD".to_string(), "DDD".to_string())),
+                ("EEE".to_string(), ("EEE".to_string(), "EEE".to_string())),
+                ("GGG".to_string(), ("GGG".to_string(), "GGG".to_string())),
+                ("ZZZ".to_string(), ("ZZZ".to_string(), "ZZZ".to_string())),
+            ]
+            .into_iter()
+            .collect(),
+        };
+
+        assert_eq!(Instruction::from_str(EXAMPLE_INPUT)?, expected);
         Ok(())
     }
 
     #[test]
-    #[ignore]
-    fn test_part2() -> Result<()> {
-        let example_input = "";
-        let expected_output = "";
+    fn test_part1() -> Result<()> {
+        let example_input = "LLR
 
-        assert_eq!(part2(example_input)?, expected_output);
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
+        assert_eq!(part1(example_input)?, 6);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<()> {
+        let example_input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        assert_eq!(part2(example_input)?, 6);
         Ok(())
     }
 }
